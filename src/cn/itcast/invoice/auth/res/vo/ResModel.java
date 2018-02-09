d90548c5927e89abf17444d651a172d3045f7404
ziotonio <35451667+ziotonio@users.noreply.github.com>
2018-02-09 16:22:58 +0100

    Add files via upload

--- src/cn/itcast/invoice/auth/res/vo/ResModel.java
+++ src/cn/itcast/invoice/auth/res/vo/ResModel.java
@@ -1,10 +1,11 @@
 package cn.itcast.invoice.auth.res.vo;
 
+import java.io.Serializable;
 import java.util.Set;
 
 import cn.itcast.invoice.auth.role.vo.RoleModel;
 
-public class ResModel {
+public class ResModel implements Serializable{
 	private Long uuid;
 	private String name;
 	//action访问路径
@@ -16,25 +17,25 @@ public class ResModel {
 	public Set<RoleModel> getRoles() {
 		return roles;
 	}
-	public void setRoles(Set<RoleModel> roles) {
+	private void setRoles(Set<RoleModel> roles) {
 		this.roles = roles;
 	}
 	public Long getUuid() {
 		return uuid;
 	}
-	public void setUuid(Long uuid) {
+	private void setUuid(Long uuid) {
 		this.uuid = uuid;
 	}
 	public String getName() {
 		return name;
 	}
-	public void setName(String name) {
+	private void setName(String name) {
 		this.name = name;
 	}
 	public String getUrl() {
 		return url;
 	}
-	public void setUrl(String url) {
+	private void setUrl(String url) {
 		this.url = url;
 	}
 	
