d60ad2b3543363a995075d6d3b32f54b80bdb586
ziotonio <35451667+ziotonio@users.noreply.github.com>
2018-02-09 17:02:02 +0100

    Add files via upload

--- src/cn/itcast/invoice/auth/res/dao/impl/ResDaoImpl.java
+++ src/cn/itcast/invoice/auth/res/dao/impl/ResDaoImpl.java
@@ -16,15 +16,15 @@ import cn.itcast.invoice.util.base.BaseQueryModel;
 public class ResDaoImpl extends BaseDaoImpl<ResModel> implements ResDao{
 	public void doQbc(DetachedCriteria dc,BaseQueryModel qm){
 		ResQueryModel rqm = (ResQueryModel) qm;
-		//TODO 添加自定义查询规则
+		//TODO æ·»åŠ è‡ªå®šä¹‰æŸ¥è¯¢è§„åˆ™
 	}
 
 	public List<String> getAllResByEmp(Long empUuid) {
 		//from tbl_res where .....
 		//from ResModel
-		//查询两个表，要求关联的数据才出现，不关联的数据不出现(6选1)
-		//内连：
-		//外连：
+		//æŸ¥è¯¢ä¸¤ä¸ªè¡¨ï¼Œè¦�æ±‚å…³è�”çš„æ•°æ�®æ‰�å‡ºçŽ°ï¼Œä¸�å…³è�”çš„æ•°æ�®ä¸�å‡ºçŽ°(6é€‰1)
+		//å†…è¿žï¼š
+		//å¤–è¿žï¼š
 		
 		//from ResModel res join res.roles  
 		//from RoleModel role join role.emps
@@ -33,11 +33,7 @@ public class ResDaoImpl extends BaseDaoImpl<ResModel> implements ResDao{
 		String hql = " select distinct res.url from ResModel res join res.roles role join role.emps em where em.uuid = ?";
 		return this.getHibernateTemplate().find(hql,empUuid);
 	}
-	public static void main(String[] args) {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml","applicationContext-res.xml");
-		ResDao dao = (ResDao) ctx.getBean("resDao");
-		System.out.println(dao.getAllResByEmp(1L));
-	}
+	
 
 	public List<String> getUrls() {
 		String hql = "select url from ResModel";
