a318fba333bd4fa3e15598b74b5f0f3c61507405
ziotonio <35451667+ziotonio@users.noreply.github.com>
2018-02-09 16:32:58 +0100

    Add files via upload

--- src/cn/itcast/invoice/invoice/storeoper/vo/StoreOperModel.java
+++ src/cn/itcast/invoice/invoice/storeoper/vo/StoreOperModel.java
@@ -1,5 +1,6 @@
 package cn.itcast.invoice.invoice.storeoper.vo;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,7 +9,7 @@ import cn.itcast.invoice.invoice.goods.vo.GoodsModel;
 import cn.itcast.invoice.invoice.order.vo.OrderModel;
 import cn.itcast.invoice.invoice.store.vo.StoreModel;
 
-public class StoreOperModel {
+public class StoreOperModel implements Serializable{
 	
 	public static final Integer STOREOPER_TYPE_OF_IN = 1;
 	public static final Integer STOREOPER_TYPE_OF_OUT = 2;
@@ -41,50 +42,50 @@ public class StoreOperModel {
 	public Long getUuid() {
 		return uuid;
 	}
-	public void setUuid(Long uuid) {
+	private void setUuid(Long uuid) {
 		this.uuid = uuid;
 	}
 	public Long getOperTime() {
 		return operTime;
 	}
-	public void setOperTime(Long operTime) {
+	private void setOperTime(Long operTime) {
 		this.operTime = operTime;
 	}
 	public Integer getNum() {
 		return num;
 	}
-	public void setNum(Integer num) {
+	private void setNum(Integer num) {
 		this.num = num;
 	}
 	public Integer getType() {
 		return type;
 	}
-	public void setType(Integer type) {
+	private void setType(Integer type) {
 		this.type = type;
 		this.typeView = typeMap.get(type);
 	}
 	public GoodsModel getGm() {
 		return gm;
 	}
-	public void setGm(GoodsModel gm) {
+	private void setGm(GoodsModel gm) {
 		this.gm = gm;
 	}
 	public EmpModel getEm() {
 		return em;
 	}
-	public void setEm(EmpModel em) {
+	private void setEm(EmpModel em) {
 		this.em = em;
 	}
 	public OrderModel getOm() {
 		return om;
 	}
-	public void setOm(OrderModel om) {
+	private void setOm(OrderModel om) {
 		this.om = om;
 	}
 	public StoreModel getSm() {
 		return sm;
 	}
-	public void setSm(StoreModel sm) {
+	private void setSm(StoreModel sm) {
 		this.sm = sm;
 	}
 	
