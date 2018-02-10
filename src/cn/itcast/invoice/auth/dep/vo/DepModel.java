24bf8b79fbe9b9b8b17deb35b473a3ffeba10f8a
ziotonio <35451667+ziotonio@users.noreply.github.com>
2018-02-10 11:17:20 +0100

    Add files via upload

--- src/cn/itcast/invoice/auth/dep/vo/DepModel.java
+++ src/cn/itcast/invoice/auth/dep/vo/DepModel.java
@@ -3,28 +3,28 @@ package cn.itcast.invoice.auth.dep.vo;
 import java.io.Serializable;
 
 public class DepModel implements Serializable{
-	private Long uuid;
+	private Long segreto;
 	
 	private String name;
 	
 	private String tele;
 	
 	public Long getUuid() {
-		return uuid;
+		return segreto;
 	}
-	private void setUuid(Long uuid) {
-		this.uuid = uuid;
+	public void setSegreto(Long segreto) {
+		this.segreto = segreto;
 	}
 	public String getName() {
 		return name;
 	}
-	private void setName(String name) {
+	public void setName(String name) {
 		this.name = name;
 	}
 	public String getTele() {
 		return tele;
 	}
-	private void setTele(String tele) {
+	public void setTele(String tele) {
 		this.tele = tele;
 	}
 	
